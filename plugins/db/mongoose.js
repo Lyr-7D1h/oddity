d8f2af2af01871a23a6b8317df09baaad5ac3ed9
ivelthoven <ivo@ivelthoven.nl>
2019-10-25 16:02:10 +0200

    added onClose and error handling added to mongoose

--- plugins/db/mongoose.js
+++ plugins/db/mongoose.js
@@ -3,13 +3,22 @@ const mongoose = require('mongoose')
 const fp = require('fastify-plugin')
 
 module.exports = fp(async instance => {
+  const connection = mongoose.createConnection(instance.config.CONNECTION_STRING, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
+  connection.on("error", err => {
+    instance.log.error(err)
+  })
+
   instance.decorate('mongoose', {
-    connection: mongoose.createConnection(instance.config.CONNECTION_STRING, {
-      useCreateIndex: true,
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    }),
+    connection: connection,
     Schema: mongoose.Schema,
     ObjectId: mongoose.Types.ObjectId
   })
+    .addHook("onClose", (fastify, done) => {
+      fastify.mongoose.connection.close(done)
+    })
 })
