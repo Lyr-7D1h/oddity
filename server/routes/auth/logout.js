4d21b9c17d2756468fcbcf7830ae38de548ff528
Ivo Velthoven <ivelthoven@protonmail.com>
2019-12-11 15:28:10 +0100

    logout bugfix

--- server/routes/auth/logout.js
+++ server/routes/auth/logout.js
@@ -1,18 +1,12 @@
 module.exports = fastify => {
-  fastify.get(
-    '/auth/logout',
-    {
-      preHandler: fastify.auth([fastify.verify.cookie])
-    },
-    (request, reply) => {
-      request.destroySession(err => {
-        if (err) {
-          fastify.log.error(err)
-          reply.send(fastify.httpErrors.internalServerError())
-        }
-        reply.clearCookie('user', { path: '/' })
-        reply.send()
-      })
-    }
-  )
+  fastify.get('/auth/logout', (request, reply) => {
+    request.destroySession(err => {
+      if (err) {
+        fastify.log.error(err)
+        reply.send(fastify.httpErrors.internalServerError())
+      }
+      reply.clearCookie('user', { path: '/' })
+      reply.success()
+    })
+  })
 }
