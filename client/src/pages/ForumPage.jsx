deec5a3676c5bb820c0c149dfd8201619033b082
Ivo Velthoven <ivelthoven@protonmail.com>
2019-12-24 20:12:25 +0100

    fix

--- client/src/pages/ForumPage.jsx
+++ client/src/pages/ForumPage.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
 import Page from '../containers/Page'
 import Breadcrumb from '../components/Breadcrumb'
 import Category from '../forum_components/Category'
