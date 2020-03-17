57dcb811d7e5d022c305914b5d600be097dfc6f4
Lyr <ivelthoven@protonmail.com>
2020-03-17 17:50:15 +0100

    Script: fix setup script module_loader order

--- setup.sh
+++ setup.sh
@@ -3,10 +3,12 @@
 CYAN='\033[0;36m'
 NY='\033[0m'
 
-
 echo -e "\n\n${CYAN}SERVER: Installing dependencies${NY}" && \
 cd server && \
 npm ci && \
+
+echo -e "\n\n${CYAN}SERVER: Executing module_loader.js to load Tables & Modules${NY}" && \
+node module_loader && \
 
 echo -e "\n\n${CYAN}SERVER: Seeding database${NY}" && \
 npx sequelize-cli db:seed:all
@@ -15,12 +17,6 @@ echo -e "\n\n${CYAN}MODULES: Linking node_modules from server to modules${NY}" &
 cd .. && \
 ln -fs "$(pwd)/server/node_modules" "$(pwd)/modules/" && \
 echo OK && \
-
-echo -e "\n\n${CYAN}SERVER: Starting server to load Tables & Modules${NY}" && \
-cd server && \
-node module_loader && \
-# timeout 5 npm start 
-
 
 echo -e "\n\n${CYAN}CLIENT: Installing dependencies${NY}" && \
 cd ../client && \
