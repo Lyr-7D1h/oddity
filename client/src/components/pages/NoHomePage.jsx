665820ef1dcb62013262d83783deb562fe79fc3c
Lyr <ivelthoven@protonmail.com>
2020-03-28 14:07:31 +0100

    Client: NoHomePage button fix

--- client/src/components/pages/NoHomePage.jsx
+++ client/src/components/pages/NoHomePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { RightOutlined } from '@ant-design/icons';
-import { Result, Button, Row } from 'antd';
+import { RightOutlined } from '@ant-design/icons'
+import { Result, Button, Row, Col } from 'antd'
 import { Link } from 'react-router-dom'
 
 export default () => {
@@ -10,24 +10,24 @@ export default () => {
       subTitle="Set a page to default/home in the admin panel"
       extra={
         <Row>
-          <Link to="/admin">
-            <Button type="primary" style={{ width: '30%', height: '50px' }}>
-              ADMIN PANEL
-              <RightOutlined />
-            </Button>
-          </Link>
-          <br />
-          <Link to="/login">
-            <Button
-              type="primary"
-              style={{ width: '30%', height: '50px', marginTop: '50px' }}
-            >
-              OR LOGIN FIRST
-              <RightOutlined />
-            </Button>
-          </Link>
+          <Col>
+            <Link to="/admin">
+              <Button type="primary" style={{ height: '50px' }}>
+                ADMIN PANEL
+                <RightOutlined />
+              </Button>
+            </Link>
+          </Col>
+          <Col>
+            <Link to="/login">
+              <Button type="primary" style={{ height: '50px' }}>
+                OR LOGIN FIRST
+                <RightOutlined />
+              </Button>
+            </Link>
+          </Col>
         </Row>
       }
     />
-  );
+  )
 }
