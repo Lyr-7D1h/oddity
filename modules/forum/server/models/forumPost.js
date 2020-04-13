504c27e01ba5d9514e6cc23c9bf06b39d45128dd
Lyr <ivelthoven@protonmail.com>
2020-04-13 20:43:10 +0200

    Server: forumPost should be unique Client: Tos +Card

--- modules/forum/server/models/forumPost.js
+++ modules/forum/server/models/forumPost.js
@@ -1,33 +1,34 @@
 module.exports = (sequelize, DataTypes) => {
-  const forumPost = sequelize.define('forumPost', {
+  const forumPost = sequelize.define("forumPost", {
     title: {
       type: DataTypes.STRING,
       allowNull: false,
-      len: [3, 40]
+      unique: true,
+      len: [3, 40],
     },
     content: {
       type: DataTypes.JSON,
       allowNull: false,
-      len: [3, 5000]
+      len: [3, 5000],
     },
     authorId: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: false,
     },
     threadId: {
       type: DataTypes.INTEGER,
-      allowNull: false
-    }
-  })
-  forumPost.associate = models => {
+      allowNull: false,
+    },
+  });
+  forumPost.associate = (models) => {
     forumPost.belongsTo(models.forumThread, {
-      as: 'posts',
-      foreignKey: 'threadId'
-    })
+      as: "posts",
+      foreignKey: "threadId",
+    });
     forumPost.belongsTo(models.user, {
-      as: 'author',
-      foreignKey: 'authorId'
-    })
-  }
-  return forumPost
-}
+      as: "author",
+      foreignKey: "authorId",
+    });
+  };
+  return forumPost;
+};
