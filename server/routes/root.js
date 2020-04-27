5fc7551066844b89c1649e32844654788f939639
Lyr <ivelthoven@protonmail.com>
2020-04-27 21:31:45 +0200

    Server: api notFound at root

--- server/routes/root.js
+++ server/routes/root.js
@@ -10,4 +10,13 @@ module.exports = async (fastify, opts) => {
       return 'Welcome to Oddity API'
     }
   )
+  fastify.all(
+    '/*',
+    {
+      permissions: fastify.PERMISSIONS.NON_SET,
+    },
+    (request, reply) => {
+      reply.notFound()
+    }
+  )
 }
