b896aa9ba2f6cf70f8b2ee030d76c7ca9940c6f5
Lyr <ivelthoven@protonmail.com>
2020-04-27 16:48:22 +0200

    Server: models_sync fix

--- server/models_sync.js
+++ server/models_sync.js
@@ -8,7 +8,7 @@ const Fastify = require('fastify')
 require('dotenv').config()
 
 const instance = Fastify({
-  dotenv: true
+  dotenv: true,
 })
 
 const schema = {
@@ -18,17 +18,17 @@ const schema = {
     DB_HOST: { type: 'string' },
     DB_NAME: { type: 'string' },
     DB_USERNAME: { type: 'string' },
-    DB_PASSWORD: { type: 'string' }
-  }
+    DB_PASSWORD: { type: 'string' },
+  },
 }
 
 instance
   .register(require('fastify-env'), {
-    schema
+    schema,
   })
   .register(require('./plugins/sequelize'))
 
-  .register(require('./db/models'))
+  .register(require('./plugins/decorators/models'))
 
 instance.ready(() => {
   console.log('Models Syncronized')
